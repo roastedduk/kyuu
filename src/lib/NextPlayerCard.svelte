42f1edef99915a2aefcd47efb542e31275c6d5b9
RoastedDuck <roastedducc@gmail.com>
2022-08-13 18:55:53 +0700

    feat: Add NextPlayerCard

---
+++ src/lib/NextPlayerCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let playerNames: string[] = []
+    $: combinedNames = playerNames.join(' & ')
+</script>
+
+<div class="w-full bg-white rounded-2xl py-3 px-6 flex flex-col">
+    <div class="text-sm">20:30</div>
+    <div class="font-bold">
+        {combinedNames}
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import PlayerCard from "../lib/PlayerCard.svelte";
+import NextPlayerCard from "../lib/NextPlayerCard.svelte";
 
 let playerNames = [
     'ayamg0rengkr1spy',
@@ -8,8 +9,16 @@ let playerNames = [
 </script>
 
 <div class="mx-4">
-    <div class="w-full flex flex-col items-center space-y-6">
-        <h1 class="text-2xl">🎮 Now playing</h1>
-        <PlayerCard playerNames="{playerNames}"/>
+    <div class="w-full flex flex-col items-center space-y-8">
+        <div class="w-full flex flex-col items-center space-y-4">
+            <h1 class="text-2xl">🎮 Now playing</h1>
+            <PlayerCard playerNames="{playerNames}"/>
+        </div>
+        <div class="w-full flex flex-col items-center space-y-4">
+            <h1 class="text-2xl">▶️ Up next (3)</h1>
+            <NextPlayerCard playerNames="{playerNames}"/>
+            <NextPlayerCard playerNames="{playerNames}"/>
+            <NextPlayerCard playerNames="{playerNames}"/>
+        </div>
     </div>
 </div>
