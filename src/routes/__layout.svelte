ec784d6fb57aa962767ad4b003dd2e57b3de9391
RoastedDuck <roastedducc@gmail.com>
2022-08-13 19:50:29 +0700

    feat: Header

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,13 @@
 	import '$lib/assets/css/app.css'
 	import '@fontsource/plus-jakarta-sans/400.css'
 	import '@fontsource/plus-jakarta-sans/700.css'
+	import Header from '$lib/Header.svelte'
 </script>
 
-<section>
+<header class="h-20 w-full fixed top-0 bg-slate-100">
+	<Header/>
+</header>
+
+<section class="max-w-lg mx-auto mt-24">
 	<slot />
 </section>
