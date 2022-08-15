2560cac06382b9041e3a378bb35305ebffda6450
RoastedDuck <roastedducc@gmail.com>
2022-08-15 23:27:53 +0700

    feat: Create QueueSheet

---
+++ src/lib/QueueSheet.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<div class="bg-white w-full p-4 rounded-t-2xl sheet fixed bottom-0 max-w-lg">
+    <div class="flex flex-col">
+        <div class="hover:cursor-grab w-full flex justify-center p-2"><div class="w-16 h-1 bg-gray-400 rounded-full"/></div>
+        <div>content</div>
+    </div>
+</div>
+
+<style>
+    .sheet {
+        box-shadow: 0 -20px 25px -5px rgb(0 0 0 / 0.1), 0 -8px 10px -6px rgb(0 0 0 / 0.1);
+    }
+</style>
