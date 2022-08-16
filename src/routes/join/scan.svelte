457088ba1d5472b34208b6a72a33ed09d523a520
Yehezkiel A <yehez30x@gmail.com>
2022-08-17 02:25:12 +0700

    feat: ability to switch camera

--- src/routes/join/scan.svelte
+++ src/routes/join/scan.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import { Html5Qrcode } from 'html5-qrcode'
 	import { onMount, onDestroy } from 'svelte'
+    import Icon, { addIcon } from '@iconify/svelte/dist/OfflineIcon.svelte'
+	import cameraRotate from '@iconify/icons-tabler/camera-rotate'
+
+    addIcon('cameraRotate', cameraRotate)
 
 	let html5QrCode: Html5Qrcode
+    let cameraId: any
+    let cameraIdPosition: number = 0 
 
 	const qrCodeSuccessCallback = (decodedText: string) => {
 		html5QrCode
@@ -23,7 +29,7 @@
 	const startScanner = async () => {
 		await html5QrCode
 			.start(
-				{ facingMode: 'environment' },
+				cameraId,
 				qrScannerConfig,
 				qrCodeSuccessCallback,
 				() => {} // ignore errors
@@ -34,9 +40,29 @@
 			})
 	}
 
-	onMount(() => {
+    const changeCamera = () => {
+        cameraIdPosition++
+        html5QrCode.stop()
+        checkCameraId()
+    }
+
+    const checkCameraId = async () => {
+        await Html5Qrcode
+                .getCameras()
+                .then(devices => {
+                    if (devices && devices.length) {
+                        if(cameraIdPosition === devices.length) {
+                            cameraIdPosition = 0
+                        }
+                        cameraId = devices[cameraIdPosition].id
+                        startScanner()
+                    }
+                })
+    }
+
+	onMount(async () => {
 		html5QrCode = new Html5Qrcode('reader')
-		startScanner()
+        await checkCameraId()
 	})
 
 	onDestroy(() => {
@@ -47,6 +73,7 @@
 <div class="w-full h-screen flex flex-col -mt-20">
 	<div class="h-4/5 bg-black flex items-center overflow-hidden relative">
 		<div class="absolute top-28 text-center w-full z-10 text-white"><h1 class="text-2xl">Scan your friend's QR code</h1></div>
+		<div on:click={changeCamera} class="absolute bottom-28 flex flex-col items-center w-full z-10 text-white "><Icon class="text-4xl p-1 bg-white/[.08] rounded-full" icon={cameraRotate} /></div>
 		<div id="reader" class="w-full max-h-full" />
 	</div>
 	<div class="flex p-6 h-1/5">
