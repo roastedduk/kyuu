e1050a47f7475eb31080798c5cd983b910674cc7
RoastedDuck <roastedducc@gmail.com>
2022-08-17 11:10:07 +0700

    refactor(QueueSheet): use BottomSheet

--- src/lib/QueueSheet.svelte
+++ src/lib/QueueSheet.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import ModalOverlay from './ModalOverlay.svelte'
+	import BottomSheet from './BottomSheet.svelte'
 
 	let sheet: Element
 	let minimized = true
@@ -9,29 +10,11 @@
 	}
 </script>
 
-<div
-	class="bg-white w-full p-4 rounded-t-2xl sheet fixed max-w-lg transition-all ease-in-out duration-500 z-20 {minimized
-		? '-bottom-20'
-		: 'bottom-0'}"
-	bind:this={sheet}
->
-	<div class="flex flex-col">
-		<div class="hover:cursor-grab w-full flex justify-center p-2">
-			<div class="w-16 h-1 bg-gray-400 rounded-full" />
-		</div>
-		<div>content</div>
-		<button on:click={expandCard}>Expand</button>
-		<p>
-			askdjaslkdjlkajsdkljalskjdlkjalsjdlkajskld jlkasjdlkjaslkd jklasjdlkajslkdjaklsjdlkaj
-			sldkjaskljdlkajslkdjlakjslkdjslkjdklasjdlkajslkjd
-		</p>
-	</div>
-</div>
-
-<ModalOverlay hidden={minimized} on:click={expandCard} />
-
-<style>
-	.sheet {
-		box-shadow: 0 -20px 25px -5px rgb(0 0 0 / 0.1), 0 -8px 10px -6px rgb(0 0 0 / 0.1);
-	}
-</style>
+<BottomSheet showHandle={true} showOverlay={!minimized} on:overlayClick={expandCard} class={minimized ? '-bottom-20' : 'bottom-0'}>
+	<div>content</div>
+	<button on:click={expandCard}>Expand</button>
+	<p>
+		askdjaslkdjlkajsdkljalskjdlkjalsjdlkajskld jlkasjdlkjaslkd jklasjdlkajslkdjaklsjdlkaj
+		sldkjaskljdlkajslkdjlakjslkdjslkjdklasjdlkajslkjd
+	</p>
+</BottomSheet>
